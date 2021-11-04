8b2a4d940b246b3d5a66178be5a4d47cc8f903a9
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-11-05 01:35:09 +0530

    Finalized Line chart

--- 04-Line-chart/src/components/Marks.js
+++ 04-Line-chart/src/components/Marks.js
@@ -1,15 +1,17 @@
-import { line, curveNatural } from 'd3';
+import { line, curveNatural } from "d3";
 const Marks = ({ data, xScale, yScale, xValue, yValue, tooltipFormat }) => {
-  return(
-    <>
-    <path
-      fill="none"
-      stroke="black"
-      d={line()
-        .x(d => xScale(xValue(d)))
-        .y(d => yScale(yValue(d)))
-        .curve(curveNatural)(data)}
-    />
+  return (
+    <g className="marks">
+      <path
+        fill="none"
+        stroke="black"
+        d={line()
+          .x((d) => xScale(xValue(d)))
+          .y((d) => yScale(yValue(d)))
+          .curve(curveNatural)(data)}
+      />
+      {/*
+   If you want line chart with dots,remove the comments 
    { data.map((d) => (
     <circle
       className="mark"
@@ -19,9 +21,9 @@ const Marks = ({ data, xScale, yScale, xValue, yValue, tooltipFormat }) => {
     >
       <title>{xValue(d)}</title>
     </circle>
-  ))}
-  
-  </>
-  )};
+  ))} */}
+    </g>
+  );
+};
 
 export default Marks;
